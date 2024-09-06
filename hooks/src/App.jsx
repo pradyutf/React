05b524cc6941eb47fe552062d05d2b70aa922fc9
pradyutf <pradyutfogla@gmail.com>
2024-09-06 15:47:51 +0530

    random todo

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,34 +1,44 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) 
+  const [todos, setTodos] = useState([
+    { id: 1, todo: 'Learn React'},
+    { id: 2, todo: 'Learn Vite'  },
+    { id: 3, todo: 'Build Something Awesome'}
+  ])
+
+  async function apiCall(){
+      const res = await fetch("https://dummyjson.com/todos?limit=5")
+      const data = await res.json()
+      console.log(data.todos);
+      setTodos([...todos,...data.todos])
+  }
+
+  useEffect( function(){
+     // setTodos([...todos,apiCall()])
+    apiCall();
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todos.map(function(todo){
+        return <Todo key={todo.id} ele={todo}></Todo>
+      })}
     </>
+  )
+}
+
+
+function Todo(props){
+  return (
+    <div>
+      <h3> {props.ele.todo}</h3>
+    </div>
   )
 }
 
