7354f20c87ea6c618b7cc1e3114bf76846ec59f9
pradyutf <pradyutfogla@gmail.com>
2024-09-18 02:02:23 +0530

    recoil wrap

--- Recoil/src/App.jsx
+++ Recoil/src/App.jsx
@@ -3,30 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { CountContext } from './context';
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { countAtom } from './store/atoms/count';
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <CountContext.Provider value={count}>
-        <Count setCount = {setCount}/>
-      </CountContext.Provider>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
     </div>
   )
 }
 
-function Count(props) {
+function Count() {
   console.log("re-render");
   return (
   <div>
+    
     <CountRenderer/>
-    <Buttons setCount = {props.setCount}/>
+    <Buttons />
+
   </div>)
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
   return <div>
    <b>
     {count}
@@ -35,19 +40,76 @@ function CountRenderer() {
 }
 
 
-function Buttons(props) {
-  const count = useContext(CountContext);
+function Buttons() {
+  const [count,setCount] = useRecoilState(countAtom);
   console.log("buttons re-rendererd");
 
   return <div>
     <button onClick={() => {
-      props.setCount(count => count + 1)
+      setCount(count + 1)
     }}>Increase</button>
 
     <button onClick={() => {
-      props.setCount(count => count - 1)
+      setCount(count - 1)
     }}>Decrease</button>
   </div>
 }
 
-export default App
+
+export default App;
+
+
+
+
+ 
+
+// using context API
+
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <CountContext.Provider value={count}>
+//         <Count setCount = {setCount}/>
+//       </CountContext.Provider>
+//     </div>
+//   )
+// }
+
+// function Count(props) {
+//   console.log("re-render");
+//   return (
+//   <div>
+//     <CountRenderer/>
+//     <Buttons setCount = {props.setCount}/>
+//   </div>)
+// }
+
+// function CountRenderer() {
+//   const count = useContext(CountContext);
+//   return <div>
+//    <b>
+//     {count}
+//    </b>
+//   </div>
+// }
+
+
+// function Buttons(props) {
+//   const count = useContext(CountContext);
+//   console.log("buttons re-rendererd");
+
+//   return <div>
+//     <button onClick={() => {
+//       props.setCount(count => count + 1)
+//     }}>Increase</button>
+
+//     <button onClick={() => {
+//       props.setCount(count => count - 1)
+//     }}>Decrease</button>
+//   </div>
+// }
+
+//export default App
